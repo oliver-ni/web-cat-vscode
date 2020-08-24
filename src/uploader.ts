135b960a5fa136a45cf00bb747ec8b05f6b47a0c
Oliver Ni <oliver.ni@gmail.com>
2020-08-24 13:41:33 -0700

    Add option to save login

--- src/uploader.ts
+++ src/uploader.ts
@@ -1,8 +1,4 @@
-import {
-    workspace,
-    window,
-    ViewColumn
-} from "vscode";
+import { workspace, window, ViewColumn } from "vscode";
 
 import { exec as execCallback } from "child_process";
 import { promisify } from "util";
@@ -10,33 +6,43 @@ import { promisify } from "util";
 const exec = promisify(execCallback);
 
 export async function upload(jarPath: string) {
-
     try {
-
         // Get submission targets
 
-        const javaPath = workspace.getConfiguration("web-cat").get<string>("java-path");
+        const javaPath = workspace
+            .getConfiguration("web-cat")
+            .get<string>("java-path");
 
         if (javaPath === undefined || javaPath === "") {
-            return window.showInformationMessage("Please set web-cat.java-path in settings.");
+            return window.showInformationMessage(
+                "Please set web-cat.java-path in settings."
+            );
         }
 
-        const submitUrl = workspace.getConfiguration("web-cat").get<string>("submit-url");
+        const submitUrl = workspace
+            .getConfiguration("web-cat")
+            .get<string>("submit-url");
 
         if (submitUrl === undefined || submitUrl === "") {
-            return window.showInformationMessage("Please set web-cat.submit-url in settings.");
+            return window.showInformationMessage(
+                "Please set web-cat.submit-url in settings."
+            );
         }
 
         const getTargetCmd: string[] = [
             javaPath,
-            "-jar", jarPath,
-            "-t", submitUrl,
-            "-l"
+            "-jar",
+            jarPath,
+            "-t",
+            submitUrl,
+            "-l",
         ];
 
         const targets = await exec(getTargetCmd.join(" "));
 
-        const target = await window.showQuickPick(targets.stdout.trim().split("\n"));
+        const target = await window.showQuickPick(
+            targets.stdout.trim().split("\n")
+        );
 
         if (target === undefined) {
             return window.showInformationMessage("Operation canceled.");
@@ -49,7 +55,7 @@ export async function upload(jarPath: string) {
             canSelectFolders: true,
             canSelectMany: false,
             defaultUri: workspace.workspaceFolders?.[0].uri,
-            openLabel: "Select Folder"
+            openLabel: "Select Folder",
         });
 
         if (dirResult === undefined) {
@@ -60,51 +66,66 @@ export async function upload(jarPath: string) {
 
         // Get username
 
-        const username = await window.showInputBox({
-            prompt: "Web-CAT Username"
-        });
+        let username = workspace
+            .getConfiguration("web-cat")
+            .get<string>("webcat-username");
 
         if (username === undefined || username === "") {
-            return window.showInformationMessage("Operation canceled.");
+            username = await window.showInputBox({
+                prompt:
+                    "Web-CAT Username (you can set this in settings if you want to save)",
+            });
+
+            if (username === undefined || username === "") {
+                return window.showInformationMessage("Operation canceled.");
+            }
         }
 
         // Get password
 
-        const password = await window.showInputBox({
-            prompt: "Web-CAT Password",
-            password: true
-        });
+        let password = workspace
+            .getConfiguration("web-cat")
+            .get<string>("webcat-password");
 
         if (password === undefined || password === "") {
-            return window.showInformationMessage("Operation canceled.");
+            password = await window.showInputBox({
+                prompt:
+                    "Web-CAT Password (you can set this in settings if you want to save)",
+                password: true,
+            });
+
+            if (password === undefined || password === "") {
+                return window.showInformationMessage("Operation canceled.");
+            }
         }
 
         // Upload
 
         const submitCmd: string[] = [
             javaPath,
-            "-jar", jarPath,
-            "-t", submitUrl,
-            "-u", '"' + username + '"',
-            "-p", '"' + password + '"',
-            "-a", '"' + target + '"',
-            '"' + dir + '"'
+            "-jar",
+            jarPath,
+            "-t",
+            submitUrl,
+            "-u",
+            '"' + username + '"',
+            "-p",
+            '"' + password + '"',
+            "-a",
+            '"' + target + '"',
+            '"' + dir + '"',
         ];
 
         const result = await exec(submitCmd.join(" "));
-
-        console.log(submitCmd.join(" "));
 
         const panel = window.createWebviewPanel(
             "submission-result",
             "Web-CAT Submission Results",
             ViewColumn.Two
         );
-        
-        panel.webview.html = result.stdout;
 
+        panel.webview.html = result.stdout;
     } catch (err) {
         window.showErrorMessage("Error submitting assignment.");
     }
-
 }
